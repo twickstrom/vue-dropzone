d56fae831e168267d260f8e9b3ccfc2793346b1d
Tim Wickstrom <tim@wickstrom.me>
2022-06-01 16:36:25 -0500

    removed old export

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
 import vueDropzone from './components/vue-dropzone.vue'
 
-export default vueDropzone
-
 export default {
   install: (app, options) => {
     app.component('vue-dropzone', vueDropzone);
